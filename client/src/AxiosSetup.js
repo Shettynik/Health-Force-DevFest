59d2b00b3e61ce6871f26c0a093b028669688b7e
Shettynik <nikshitashetty2612@gmail.com>
2021-08-16 16:05:10 +0530

    revert changes

--- client/src/AxiosSetup.js
+++ client/src/AxiosSetup.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-// const baseURL = 'http://localhost:5000';
-const baseURL = 'https://ec2-3-238-174-201.compute-1.amazonaws.com/';
+const baseURL = 'http://localhost:5000';
 
 export const axiosInstance = axios.create({
     baseURL: baseURL
--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cors({
-    // origin: ['http://localhost:3000'],
-    ortigin: ['https://ec2-3-238-174-201.compute-1.amazonaws.com/'],
+    origin: ['http://localhost:3000'],
     credentials: true
 }));
 app.use(cookieParser());
